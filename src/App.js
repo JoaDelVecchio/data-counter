ab413abc05d103b49743dc5787b30cf780dbf557
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-08-12 09:45:51 -0300

    Make counter structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter
+        step={step}
+        setStep={setStep}
+        count={count}
+        setCount={setCount}
+      />
+      <Message count={count} />
     </div>
   );
 }
 
+const Counter = ({ step, setStep, count, setCount }) => {
+  const incrementStep = () => setStep((s) => s + 1);
+  const decrementStep = () => setStep((s) => s - 1);
+  const incrementCount = () => setCount((s) => s + step);
+  const decrementCount = () => setCount((s) => s - step);
+
+  return (
+    <div>
+      <Buttons
+        text="step"
+        decrement={decrementStep}
+        increment={incrementStep}
+        variableToShow={step}
+      />
+      <Buttons
+        text="count"
+        decrement={decrementCount}
+        increment={incrementCount}
+        variableToShow={count}
+      />
+    </div>
+  );
+};
+
+const Message = ({ count }) => {
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  return (
+    <p>
+      {count === 0
+        ? "today is"
+        : count > 0
+        ? `${count} days from today is`
+        : `${Math.abs(count)} days ago was`}{" "}
+      {date.toDateString()}
+    </p>
+  );
+};
+
 export default App;
+
+const Buttons = ({ text, decrement, increment, variableToShow }) => {
+  return (
+    <div>
+      <button onClick={() => decrement()}>-</button>
+      <span>
+        {text}
+        {variableToShow}
+      </span>
+      <button onClick={() => increment()}>+</button>
+    </div>
+  );
+};
